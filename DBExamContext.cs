daedccc3fe2e7b5eb7cdb593544784d5a5cd2e0a
FelixvL <digitaleservice@gmail.com>
2020-09-18 14:54:36 +0200

    start met vraag

---
+++ DBExamContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBExamenVragenAppDB
+{
+    public class DBExamContext : DbContext
+    {
+        public DbSet<Vraag> vragen { get; set; }
+    }
+}
---
+++ Vraag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBExamenVragenAppDB
+{
+    public class Vraag
+    {
+        [Key]
+        public int VraagID { get; set; }
+        public string VraagTekst { get; set; }
+        public string Antwoord { get; set; }
+        public string Toelichting { get; set; }
+    }
+}
